ff5f513b63ecc6a15be6602b28c5cb9750113878
Nikolas Poniros <nikolas@poniros.de>
2016-11-19 13:16:23 +0100

    reorganize code

--- bin/main.js
+++ bin/main.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 'use strict';
 
-
 const argv = require('yargs').argv;
 
 const action = argv._[0];
 
 const isProd = argv.prod;
 
-const generate = require('../lib/generate.js');
+const generate = require('../lib/actions/generate.js');
 
 const actions = {
   generate
 };
 
 const basePath = process.cwd();
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Possibly Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
 
 if (actions[action]) {
   actions[action](basePath, isProd);
