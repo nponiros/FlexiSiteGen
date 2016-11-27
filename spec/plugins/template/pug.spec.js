cdb4ade064a2fa68e51154000d982c5213eb1e02
Nikolas Poniros <nikolas@poniros.de>
2016-11-27 13:06:40 +0100

    Add pug template spec

---
+++ spec/plugins/template/pug.spec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const registration = require('../../../lib/plugins/template/pug');
+
+const pug = require('pug');
+
+describe('pug template processor', () => {
+  let pugTemplateProcessor;
+  let renderSpy = jasmine.createSpy('render spy');
+
+  beforeAll(() => {
+    registration((type, name, extensions, fn) => {
+      pugTemplateProcessor = fn;
+    });
+  });
+
+  beforeEach(() => {
+    spyOn(pug, 'compile').and.returnValue(renderSpy);
+  });
+
+  it('should add a filename property to the options object', () => {
+    pugTemplateProcessor('', { filenameWithPath: 'foobar' }, {});
+
+    expect(pug.compile).toHaveBeenCalledWith('', {
+      filenameWithPath: 'foobar',
+      filename: 'foobar',
+    });
+  });
+
+  it('should call render spy with the given template variables', () => {
+    pugTemplateProcessor('', { filenameWithPath: 'foobar' }, { vars: 'foo' });
+
+    expect(renderSpy).toHaveBeenCalledWith({ vars: 'foo' });
+  });
+});
