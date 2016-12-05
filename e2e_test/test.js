14b5d58f53e557c900ced69c4ea7d51325b97421
Nikolas Poniros <nikolas@poniros.de>
2016-12-05 13:41:21 +0100

    fix e2e test

--- e2e_test/test.js
+++ e2e_test/test.js
@@ -1,21 +1,10 @@
-const fs = require('fs');
-const fsExtra = require('fs-extra');
+'use strict';
+
+const fs = require('fs-extra');
 const path = require('path');
 const assert = require('assert');
 
 const generateFn = require('../lib/actions/generate');
-
-/*
-
- E2E:
-
- * need to call builder on the correct test_site -> DONE
- * need to create a result => use public -> DONE
- * need to know when building is done -> use done function => DONE
- * need to compare public with expectation
- * recurse, read each file and compare as buffer
-
- */
 
 const basePath = process.cwd();
 const testBasePath = basePath + '/e2e_test/test_site';
@@ -24,24 +13,20 @@ const publicDir = testBasePath + '/public';
 generateFn(testBasePath, false, (_, n) => {
   n();
 
-  var walkSync = function(dir, filelist) {
-    var path = path || require('path');
-    var fs = fs || require('fs'),
-        files = fs.readdirSync(dir);
-    filelist = filelist || [];
-    files.forEach(function(file) {
+  function getFilesList(dir, filelist) {
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
       if (fs.statSync(path.join(dir, file)).isDirectory()) {
-        filelist = walkSync(path.join(dir, file), filelist);
-      }
-      else {
+        filelist = getFilesList(path.join(dir, file), filelist);
+      } else {
         filelist.push(path.join(dir, file));
       }
     });
     return filelist;
-  };
+  }
 
-  const result = walkSync(publicDir, []);
-  const expectation = walkSync(expectationDir, []);
+  const result = getFilesList(publicDir, []);
+  const expectation = getFilesList(expectationDir, []);
 
   assert(result.length === expectation.length, 'The number of files in the directories do not match');
 
@@ -60,5 +45,5 @@ generateFn(testBasePath, false, (_, n) => {
     assert(expectedFiles[key] === resultFiles[key], `The files do not match. File was: ${key}`)
   });
 
-  fsExtra.removeSync(publicDir);
+  fs.removeSync(publicDir);
 });
