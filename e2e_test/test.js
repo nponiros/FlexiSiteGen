efadc07ce082ece342191a4b8511e4d4365cbdfc
Nikolas Poniros <nikolas@poniros.de>
2016-12-06 17:02:58 +0100

    add test for --prod mode

--- e2e_test/test.js
+++ e2e_test/test.js
@@ -8,29 +8,73 @@ const generateFn = require('../lib/actions/generate');
 const basePath = process.cwd();
 const testBasePath = basePath + '/e2e_test/test_site';
 const expectationDir = testBasePath + '/expectation';
+const expectationProdDir = testBasePath + '/expectation_prod';
 const publicDir = testBasePath + '/public';
-generateFn(testBasePath, false, (_, n) => {
-  n();
 
-  const result = fs.walkSync(publicDir);
-  const expectation = fs.walkSync(expectationDir, []);
+function t1(done) {
+  console.log('generate test')
+  generateFn(testBasePath, false, (_, n) => {
+    n();
 
-  assert(result.length === expectation.length, 'The number of files in the directories do not match');
+    const result = fs.walkSync(publicDir);
+    const expectation = fs.walkSync(expectationDir, []);
 
-  const resultFiles = result.reduce((map, filePath) => Object.assign(
-    map,
-    { [filePath]: fs.readFileSync(filePath, { encoding: 'utf8' }) }
-  ), {});
+    assert(result.length === expectation.length, 'The number of files in the directories do not match');
 
-  const expectedFiles = expectation.reduce((map, filePath) => Object.assign(
-      map,
-      { [filePath.replace(expectationDir, publicDir)]: fs.readFileSync(filePath, { encoding: 'utf8' }) }
-  ), {});
+    const resultFiles = result.reduce((map, filePath) => Object.assign(
+        map,
+        { [filePath]: fs.readFileSync(filePath, { encoding: 'utf8' }) }
+    ), {});
 
-  const keys = Object.keys(expectedFiles);
-  keys.forEach((key) => {
-    assert(expectedFiles[key] === resultFiles[key], `The files do not match. File was: ${key}`);
+    const expectedFiles = expectation.reduce((map, filePath) => Object.assign(
+        map,
+        { [filePath.replace(expectationDir, publicDir)]: fs.readFileSync(filePath, { encoding: 'utf8' }) }
+    ), {});
+
+    const keys = Object.keys(expectedFiles);
+    keys.forEach((key) => {
+      assert(expectedFiles[key] === resultFiles[key], `The files do not match. File was: ${key}`);
+    });
+
+    fs.removeSync(publicDir);
+
+    done();
   });
+}
 
-  fs.removeSync(publicDir);
+function t2(done) {
+  console.log('generate test production mode');
+  generateFn(testBasePath, true, (_, n) => {
+    n();
+
+    const result = fs.walkSync(publicDir);
+    const expectation = fs.walkSync(expectationProdDir, []);
+
+    assert(result.length === expectation.length, 'The number of files in the directories do not match');
+
+    const resultFiles = result.reduce((map, filePath) => Object.assign(
+        map,
+        { [filePath]: fs.readFileSync(filePath, { encoding: 'utf8' }) }
+    ), {});
+
+    const expectedFiles = expectation.reduce((map, filePath) => Object.assign(
+        map,
+        { [filePath.replace(expectationProdDir, publicDir)]: fs.readFileSync(filePath, { encoding: 'utf8' }) }
+    ), {});
+
+    const keys = Object.keys(expectedFiles);
+    keys.forEach((key) => {
+      assert(expectedFiles[key] === resultFiles[key], `The files do not match. File was: ${key}`);
+    });
+
+    fs.removeSync(publicDir);
+
+    done();
+  });
+}
+
+t1(() => {
+  t2(() => {
+    console.log('All tests done');
+  });
 });
